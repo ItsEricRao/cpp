bab8d47dfaae4f7780241c72cccb58cfcbaebff4
ItsEricRao <EricRaoMINECRAFT@outlook.com>
2024-10-20 21:51:51 +0800

    fixed bugs in b3968

--- training/10.20/b3968.cpp
+++ training/10.20/b3968.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN = 1e4;
+const int MAXN = 1e5;
 int n;
 
 struct Student {
-    int chn, mat, eng;
-    int tot, tot2, mx;
-    int ord, rank;
+    int chn=-1, mat=-1, eng=-1;
+    int tot=-1, tot2=-1, mx=-1;
+    int ord=0, rank=-1;
     bool flag = false;
 } s[MAXN];
 
 bool cmp1(Student x, Student y) {
     if (x.tot != y.tot) return x.tot > y.tot;
-    else if (x.tot == y.tot) return x.tot2 > y.tot2;
-    else if (x.tot == y.tot && x.tot2 == y.tot2) return x.mx > y.mx;
-    else return false;
+    else if (x.tot2 != y.tot2) return x.tot2 > y.tot2;
+    else return x.mx >= y.mx;
 }
 
 bool cmp2(Student x, Student y) {
